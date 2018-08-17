9686244a865f92f56fc1e929edb651c21dda0d0f
illustietor <illustietor@gmail.com>
2018-08-17 18:31:08 +0200

    Minijuegos con menús y otras mierdas

---
+++ Assets/EL_JUEGO (No tocar)/MenusTochos/Scripts/ContinuarJuego.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuarJuego : MonoBehaviour {
+
+    public GameObject menuPausaMj;
+
+    public void QuitarPausa()
+    {
+        menuPausaMj.SetActive(false);
+        Time.timeScale = 1;
+
+    }
+}
---
+++ Assets/EL_JUEGO (No tocar)/MenusTochos/Scripts/ScriptSalirJuego.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptSalirJuego : MonoBehaviour {
+
+	public void VolverUltimaEscena ()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetString("EscenaQueCargaJuego"));
+    }
+}
