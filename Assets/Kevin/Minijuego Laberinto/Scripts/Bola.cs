8f26e0bc1b9edba92ae1429c5edaa78ca5e00246
illustietor <illustietor@gmail.com>
2018-08-18 02:50:32 +0200

    VENGA METELE TOA ESA POLLA

--- Assets/Kevin/Minijuego Laberinto/Scripts/Bola.cs
+++ Assets/Kevin/Minijuego Laberinto/Scripts/Bola.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bola : MonoBehaviour {
+
+    bool ponerPausa = true;
+    public GameObject menuPausaMj;
+    public GameObject menuFinalMj;
+
     public float velocidad = 5;
 
     void FixedUpdate()
@@ -11,5 +16,26 @@ public class Bola : MonoBehaviour {
         float vertical = Input.GetAxis("Vertical");
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal * velocidad, vertical * velocidad);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && ponerPausa)
+        {
+            PausaJuego();
+        }
+    }
+
+    public void PausaJuego()
+    {
+        Time.timeScale = 0;
+        menuPausaMj.SetActive(true);
+        Debug.Log("Juego Pausado");
+
+    }
+
+    public void PausaFalse()
+    {
+        ponerPausa = false;
     }
 }
--- Assets/Kevin/Minijuego Laberinto/Scripts/Final.cs
+++ Assets/Kevin/Minijuego Laberinto/Scripts/Final.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Final : MonoBehaviour {
-
+    public GameObject player;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Time.timeScale = 0;            
+        player.GetComponent<Bola>().enabled = false;
+        //Time.timeScale = 0;
+        player.GetComponent<Bola>().PausaFalse();
+        Time.timeScale = 0;
+        player.GetComponent<Bola>().menuFinalMj.SetActive(true);
     }
 }
